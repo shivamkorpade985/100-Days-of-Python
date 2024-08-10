8484f6eb3f99f648af36012269e0e15c76263c17
shivamkorpade985 <158841085+shivamkorpade985@users.noreply.github.com>
2024-08-11 01:47:19 +0530

    Add files via upload

---
+++ maxbid.py
@@ -0,0 +1,32 @@
+def find_highest_bidder(bidding_dictionary):
+    winner = ''
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    
+    print(f"Winner is {winner},with the bid of ${highest_bid}.")
+
+
+
+
+bids = {}
+countinue_bidding = True
+while countinue_bidding:
+    name = input("Enter name:")
+    price = int(input("Enter bid:$"))
+    bids[name] = price
+    should_continue = input("Type 'yes' or 'no' whether the next bidder wants to bid?").lower()
+    if should_continue == 'no':
+        countinue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == 'yes':
+        print("\n" * 20)
+
+
+
+
+
+ 
