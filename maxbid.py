d2541d8604e342141696ed9328053dc36aa20731
shivamkorpade985 <158841085+shivamkorpade985@users.noreply.github.com>
2024-08-11 01:55:07 +0530

    Update maxbid.py

--- maxbid.py
+++ maxbid.py
@@ -1,16 +1,11 @@
-def find_highest_bidder(bidding_dictionary):
-    winner = ''
+
     highest_bid = 0
     for bidder in bidding_dictionary:
         bid_amount = bidding_dictionary[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
-    
     print(f"Winner is {winner},with the bid of ${highest_bid}.")
-
-
-
 
 bids = {}
 countinue_bidding = True
@@ -24,9 +19,3 @@ while countinue_bidding:
         find_highest_bidder(bids)
     elif should_continue == 'yes':
         print("\n" * 20)
-
-
-
-
-
- 
